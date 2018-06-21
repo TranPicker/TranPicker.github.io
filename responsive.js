04efb4a71b3bc45e21999bababa232e4968b243c
fullstackdeveloperangular-php <microsoftofmy.boss@gmail.com>
2018-06-21 15:50:15 +0700

    menu responsive

--- responsive.js
+++ responsive.js
@@ -1,14 +1,33 @@
 function openNav(){
-	// alert("javascript is working.");
-	var navbar = document.getElementById("navbar");
-	var hasClass = navbar.getAttribute("class");
-	if(hasClass === "menu")
-	{
-		navbar.setAttribute("class","menu responsive");
-		
-		// navbar.style.animation = "animation: hienra 1.5s forwards;"
-	}else{
-		navbar.setAttribute("class","menu");
-	}
+	// doi button
+	document.getElementById("open-button").style.display= "none";
+	document.getElementById("close-button").style.display= "inline-block";
+	// mo rong menu
+	document.getElementById("navbar-respon").style.width = "100%";
+	// var navbar_item = document.getElementsByClassName("navbar-item");
+	// console.log(navbar_item);
+	// for (var i = 0;i < navbar_item.length ; i++) {
+	// 	var child_node= navbar_item[i].childNodes[0];
+	// 	changeWidth(child_node,"100px");
+	// }
 
+	
+
+}
+function closeNav(){
+	// doi button
+	document.getElementById("open-button").style.display= "inline-block";
+	document.getElementById("close-button").style.display= "none";
+
+	document.getElementById("navbar-respon").style.width = "0%";
+	// var navbar_item = document.getElementsByClassName("navbar-item");
+	// console.log(navbar_item);
+	// for (var i = 0;i < navbar_item.length ; i++) {
+	// 	var child_node= navbar_item[i].childNodes[0];
+	// 	changeWidth(child_node,"0");
+	// }
+
+}
+function changeWidth(obj,w){
+	obj.style.width = w;
 }
